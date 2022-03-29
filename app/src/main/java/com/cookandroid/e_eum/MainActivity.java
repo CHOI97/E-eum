8aa569fc5980981edcf878857577d96aa0eb1fbe
CHOI97 <chlwjrh@gmail.com>
2022-03-29 16:34:05 +0900

    first commit

---
+++ app/src/main/java/com/cookandroid/e_eum/MainActivity.java
@@ -0,0 +1,14 @@
+package com.cookandroid.e_eum;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
