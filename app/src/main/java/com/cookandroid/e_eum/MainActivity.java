97c5f192863cbf7ca67b3850a7ee83df9ad380b2
CHOI97 <chlwjrh@gmail.com>
2022-04-06 18:57:35 +0900

    Fix surfaceview

--- app/src/main/java/com/cookandroid/e_eum/MainActivity.java
+++ app/src/main/java/com/cookandroid/e_eum/MainActivity.java
@@ -3,57 +3,34 @@ package com.cookandroid.e_eum;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Context;
-import android.graphics.Camera;
+import android.hardware.Camera;
+import android.graphics.PixelFormat;
 import android.os.Bundle;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
-import android.view.ViewGroup;
 import android.widget.Button;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
     Camera camera;
-    Button btn_test1;
+    Button btn_test_start,btn_test_end;
     SurfaceView sfv_test1;
     SurfaceHolder sfv_holder;
+    boolean previewing = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btn_test1 = findViewById(R.id.btn_test1);
+        btn_test_start = findViewById(R.id.btn_test_start);
+        btn_test_end = findViewById(R.id.btn_test_end);
         sfv_test1 = findViewById(R.id.sfv_test1);
-    }
-    class Preview extends ViewGroup implements SurfaceHolder.Callback {
 
-        Preview(Context context) {
-            super(context);
-
-            sfv_test1 = new SurfaceView(context);
-            addView(sfv_test1);
-            sfv_holder = sfv_test1.getHolder();
-            sfv_holder.addCallback(this);
-            sfv_holder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
-        }
-
-        @Override
-        protected void onLayout(boolean b, int i, int i1, int i2, int i3) {
-
-        }
-
-        @Override
-        public void surfaceCreated(@NonNull SurfaceHolder surfaceHolder) {
-
-        }
-
-        @Override
-        public void surfaceChanged(@NonNull SurfaceHolder surfaceHolder, int i, int i1, int i2) {
-
-        }
-
-        @Override
-        public void surfaceDestroyed(@NonNull SurfaceHolder surfaceHolder) {
-
-        }
+        getWindow().setFormat(PixelFormat.UNKNOWN);
+        sfv_test1 = (SurfaceView)findViewById(R.id.sfv_test1);
+        sfv_holder = sfv_test1.getHolder();
+//        sfv_holder.addCallback(this);
+        sfv_holder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
     }
 }
