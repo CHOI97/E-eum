f6b9ce1302b2526a80896b2973a741d150e57673
CHOI97 <chlwjrh@gmail.com>
2022-04-09 05:46:54 +0900

    CameraX추가 및 코드 수정

--- app/src/main/java/com/cookandroid/e_eum/MainActivity.kt
+++ app/src/main/java/com/cookandroid/e_eum/MainActivity.kt
@@ -1,30 +1,111 @@
 package com.cookandroid.e_eum
 
+import android.Manifest
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
-import android.view.SurfaceView
-import android.view.SurfaceHolder
 import android.os.Bundle
-import com.cookandroid.e_eum.R
-import android.graphics.PixelFormat
-import android.hardware.Camera
-import android.view.View
+import android.util.Log
 import android.widget.Button
+import android.widget.Toast
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageCapture
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.camera.view.PreviewView
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import java.io.File
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
 
 class MainActivity : AppCompatActivity() {
-    var camera: Camera? = null
-    var btn_test_start: Button? = null
-    var btn_test_end: Button? = null
-    var sfv_test1: SurfaceView? = null
-    var sfv_holder: SurfaceHolder? = null
-    var previewing = false
+
+//    val btn_test_end: Button = findViewById(R.id.btn_test_end)
+//    private lateinit var viewFinder: PreviewView
+
+    private var imageCapture: ImageCapture? = null
+    private lateinit var outputDirectory: File
+    private lateinit var cameraExecutor: ExecutorService
+    private lateinit var viewFinder: PreviewView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        btn_test_start = findViewById(R.id.btn_test_start)
-        btn_test_end = findViewById(R.id.btn_test_end)
-        sfv_test1 = findViewById(R.id.sfv_test1)
-        window.setFormat(PixelFormat.UNKNOWN)
-        sfv_test1 = findViewById<View>(R.id.sfv_test1) as SurfaceView
-        sfv_holder = sfv_test1!!.holder
+        viewFinder = findViewById(R.id.pre_test1)
+        val btn_test_start: Button = findViewById(R.id.btn_test_start)
+        Log.d("error","error@@@@@@@@@@@")
+        // Request camera permissions
+        if (allPermissionsGranted()) {
+            startCamera()
+        } else {
+            ActivityCompat.requestPermissions(
+                this, REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS)
+        }
+
+        // Set up the listener for take photo button
+        btn_test_start.setOnClickListener { takePhoto() }
+
+        outputDirectory = getOutputDirectory()
+
+        cameraExecutor = Executors.newSingleThreadExecutor()
+    }
+
+
+    private fun takePhoto() {}
+
+    private fun startCamera() {
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
+
+        cameraProviderFuture.addListener(Runnable {
+            // Used to bind the lifecycle of cameras to the lifecycle owner
+            val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
+
+            // Preview
+            val preview = Preview.Builder()
+                .build()
+                .also {
+                    it.setSurfaceProvider(viewFinder.surfaceProvider)
+                }
+
+            // Select back camera as a default
+            val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+
+            try {
+                // Unbind use cases before rebinding
+                cameraProvider.unbindAll()
+
+                // Bind use cases to camera
+                cameraProvider.bindToLifecycle(
+                    this, cameraSelector, preview)
+
+            } catch(exc: Exception) {
+                Log.e(TAG, "Use case binding failed", exc)
+            }
+
+        }, ContextCompat.getMainExecutor(this))
+    }
+
+    private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
+        ContextCompat.checkSelfPermission(
+            baseContext, it) == PackageManager.PERMISSION_GRANTED
+    }
+
+    private fun getOutputDirectory(): File {
+        val mediaDir = externalMediaDirs.firstOrNull()?.let {
+            File(it, resources.getString(R.string.app_name)).apply { mkdirs() } }
+        return if (mediaDir != null && mediaDir.exists())
+            mediaDir else filesDir
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        cameraExecutor.shutdown()
+    }
+
+    companion object {
+        private const val TAG = "CameraXBasic"
+        private const val FILENAME_FORMAT = "yyyy-MM-dd-HH-mm-ss-SSS"
+        private const val REQUEST_CODE_PERMISSIONS = 10
+        private val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
     }
 }
