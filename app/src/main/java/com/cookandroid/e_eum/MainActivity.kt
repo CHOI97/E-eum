c4fe833ee6d05458c16d2cb1335de88788ff8cf5
CHOI97 <chlwjrh@gmail.com>
2022-05-22 19:49:46 +0900

    merge layout

---
+++ app/src/main/java/com/cookandroid/e_eum/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.cookandroid.e_eum
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.cookandroid.e_eum.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_result)
+    }
+}
